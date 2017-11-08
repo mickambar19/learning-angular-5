afcae42b003a47a0850849eefb9d7ddf452c51ea
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2017-11-08 12:36:11 -0600

    Task 3 completed using angular built in directives
    Took 29 minutes

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  toggleState = false;
+  log = [];
+  updateToggleState(){
+    if (this.toggleState){
+      this.log.push(Date());
+    }
+
+    this.toggleState = !this.toggleState
+  }
+
+  getBG(index){
+    return this.isFifthOrGreater(index) ? 'blue': '';
+  }
+
+  isFifthOrGreater( index ){
+    return index >= 4? true: false;
+  }
 }
